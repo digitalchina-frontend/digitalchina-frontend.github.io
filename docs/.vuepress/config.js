cdde30ae82895e048e7b11e607da131c74d38195
范轶洁 <fanyjb@digitalchina.com>
2022-04-24 16:35:37 +0800

    侧边栏配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,8 @@ module.exports = {
   title: "神码前端",
   description: "什么前端?",
   themeConfig: {
+    sidebar: "auto",
+    sidebarDepth: 2,
     nav: [
       { text: "Home", link: "/" },
       { text: "Odoo", link: "/odoo/" },
