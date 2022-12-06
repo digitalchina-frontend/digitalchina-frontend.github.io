12438fcfb32a9f9f40ae43d147d65012f8032b96
肖子立 <xiaozlb@digitalchina.com>
2022-12-06 10:50:33 +0800

    📦 NEW: config meta data

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: '现代前端开发指南',
+      description: '大道至简，让前端开发更简单',
+    },
+  },
+})
