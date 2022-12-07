d8c9b86a6f55ff933cee3d9b97a2ef6d4adefaa8
Anyhow Lee <Anyhow_Lee@163.com>
2022-12-07 10:14:01 +0800

    feat:添加《基于AntD 的样式定制》文章

--- docs/.vuepress/configs/navbar/zh.js
+++ docs/.vuepress/configs/navbar/zh.js
@@ -1,15 +1,19 @@
 export const navbarZh = [
   {
-    text: '设计',
+    text: "设计",
     children: [],
   },
   {
-    text: '开发',
+    text: "开发",
     children: [
       {
-        text: 'VuePress V2 搭建过程记录',
-        link: '/develop/develop-log/',
+        text: "VuePress V2 搭建过程记录",
+        link: "/develop/develop-log/",
+      },
+      {
+        text: "基于AntD 的样式定制",
+        link: "/develop/about-antd-css/costom-antd-css",
       },
     ],
   },
-]
+];
