6a85afc582e45958273231d714e87e9c67c47867
GAOYHJ <gaoyhj@digitalchina.com>
2022-12-26 18:05:38 +0800

    📖 DOC: 新增《可选链操作符》《空值合并运算符》

--- docs/.vuepress/configs/sidebar/zh.js
+++ docs/.vuepress/configs/sidebar/zh.js
@@ -73,6 +73,14 @@ export const sidebarZh = {
           text: '箭头函数',
           link: '/develop/javascript/es6/syntax/arrow-function',
         },
+        {
+          text: '空值合并运算符',
+          link: '/develop/javascript/es6/syntax/空值合并运算符.md',
+        },
+        {
+          text: '可选链操作符',
+          link: '/develop/javascript/es6/syntax/可选链操作符.md',
+        },
       ],
     },
     {
