7fe0f1ac04115dcc6b0b072ebab600ee68caf774
肖子立 <xiaozlb@digitalchina.com>
2022-12-28 14:40:03 +0800

    📦 NEW: 集成 git plugin

--- docs/.vuepress/theme/index.js
+++ docs/.vuepress/theme/index.js
@@ -1,8 +1,14 @@
 import { path } from '@vuepress/utils'
+import { gitPlugin } from '@vuepress/plugin-git'
 
 export const DcTheme = (options) => (app) => {
   return {
     name: 'vuepress-theme-dc',
     clientConfigFile: path.resolve(__dirname, 'client.js'),
+    plugins: [
+      gitPlugin({
+        // 配置项
+      }),
+    ],
   }
 }
