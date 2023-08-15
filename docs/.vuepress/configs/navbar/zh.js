0aefeeddc115ef1fb72ba894225495d54debb2c2
fancn21th <fancn21th@gmail.com>
2023-08-15 15:09:15 +0800

    结构更新

--- docs/.vuepress/configs/navbar/zh.js
+++ docs/.vuepress/configs/navbar/zh.js
@@ -1,6 +1,10 @@
 export const navbarZh = [
   {
-    text: '设计',
+    text: '流程',
+    children: [],
+  },
+  {
+    text: '设计系统',
     children: [
       {
         text: '调研文档及学习输出',
@@ -13,7 +17,7 @@ export const navbarZh = [
     ],
   },
   {
-    text: '开发',
+    text: '框架',
     children: [
       {
         text: '基于AntD 的样式定制',
@@ -54,19 +58,42 @@ export const navbarZh = [
     ],
   },
   {
+    text: '编程能力',
+    children: [],
+  },
+  {
+    text: '人工智能',
+    children: [],
+  },
+  {
     text: 'Matrix',
     children: [
-      '/develop/matrix/CONTRIBUTING',
       {
-        text: 'Matrix - 小程序',
-        link: 'https://digitalchina-frontend.github.io/matrix-miniapp/',
+        text: '如何贡献',
+        children: [
+          {
+            text: 'VuePress V2 搭建过程记录',
+            link: '/develop/vuejs/vuepressv2-build-process-record',
+          },
+          '/develop/matrix/CONTRIBUTING',
+          '/develop/git/contributiong/',
+        ],
       },
       {
-        text: 'Matrix - 中后台',
-        link: 'https://digitalchina-frontend.github.io/matrix-admin/',
+        text: '子站点',
+        children: [
+          {
+            text: 'Matrix - 小程序',
+            link: 'https://digitalchina-frontend.github.io/matrix-miniapp/',
+          },
+          {
+            text: 'Matrix - 中后台',
+            link: 'https://digitalchina-frontend.github.io/matrix-admin/',
+          },
+        ],
       },
       {
-        text: 'Matrix - 大屏',
+        text: '大屏',
         children: [
           {
             text: 'V1',
@@ -79,13 +106,5 @@ export const navbarZh = [
         ],
       },
     ],
-  },
-  {
-    text: '贡献指南',
-    link: '/develop/git/contributiong/',
-  },
-  {
-    text: 'VuePress V2 搭建过程记录',
-    link: '/develop/vuejs/vuepressv2-build-process-record',
   },
 ]
